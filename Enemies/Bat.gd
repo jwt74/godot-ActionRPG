6c26ee0c06efcb0f4ad739b25ca5e6bd77361185
= <=>
2020-11-15 07:23:29 -0500

    Still in part 13

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,4 +1,11 @@
 extends KinematicBody2D
 
+var knockback = Vector2.ZERO
 
-
+func _physics_process(delta):
+	move_and_slide(knockback)
+	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	
+func _on_Hurtbox_area_entered(area):
+	knockback = Vector2.RIGHT * 200
+#	queue_free()
