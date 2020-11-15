061872cddc7d898ad4185764a7cdbad7f0d967cd
jwt74 <joseph.tucker@gmail.com>
2020-11-15 17:34:08 -0500

    Part 17 complete - Player Stats + Enemy Attack

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -20,6 +20,7 @@ var state = CHASE
 onready var stats = $Stats
 onready var playerDectectionZone = $PlayerDectectionZone
 onready var sprite = $AnimatedSprite
+onready var hurtbox = $Hurtbox
 
 func createEnemyDeathEffect():
 	var enemyDeathEffect = EnemyDeathEffect.instance()
@@ -54,6 +55,8 @@ func seek_player():
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
+	hurtbox.create_hit_effect()
+	
 
 func _on_Stats_no_health():
 	queue_free()
