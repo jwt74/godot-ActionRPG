fa69f7bb4ecc73fd7885e5aa1bde7239b131b5bb
jwt74 <joseph.tucker@gmail.com>
2020-11-15 08:20:12 -0500

    Part 13 complete - Knockback and Enemy Bat

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -7,5 +7,5 @@ func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	
 func _on_Hurtbox_area_entered(area):
-	knockback = Vector2.RIGHT * 200
+	knockback = area.knockback_vector * 120
 #	queue_free()
