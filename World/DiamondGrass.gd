b89f50410a3b1e5dbe6dee91272206e0b0be94d3
jwt74 <joseph.tucker@gmail.com>
2020-11-22 16:39:57 -0500

    Add a commit message

---
+++ World/DiamondGrass.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+const GrassEffect = preload("res://Effects/DiamondGrassEffect.tscn")
+var stats = PlayerStats
+
+func create_grass_effect():
+	var grassEffect = GrassEffect.instance()
+	get_parent().add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	stats.health = stats.max_health
+	queue_free()
